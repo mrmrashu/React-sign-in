afefe7d29ab3226e0faece473f4328409c6e3c17
mrmrashu <105367150+mrmrashu@users.noreply.github.com>
2022-05-11 22:39:35 +0530

    Add files via upload

---
+++ src/components/Titles/Titles.js
@@ -0,0 +1,9 @@
+import "./Titles.css"
+
+const Titles = (props) => {
+	return (
+		<Titles className="Titles">{`${props.Content}`}</Titles>
+		)
+}
+
+export {Titles};
---
+++ src/components/text-box/text-box.js
@@ -0,0 +1,8 @@
+import "./text-box.css"
+const TextBox = (props) => {
+	return (
+			<input autoFocus type="text" className="workonfocus native-textbox"/>
+		)
+}
+
+export {TextBox};
